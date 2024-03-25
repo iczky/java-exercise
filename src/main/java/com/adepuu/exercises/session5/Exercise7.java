ca5fe1ba6e0dfd30830b27f425789bc44acc532e
iczky <fiqra22@gmail.com>
2024-03-25 13:25:46 +0700

    feat: Complete Exercise 7

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Exercise7 {
     /**
      * Java String Program to Check Anagram
@@ -13,6 +16,38 @@ public class Exercise7 {
      * Output: false
      */
     public static void main(String[] args) {
+        System.out.println(checkAnagram("rat", "car"));
+    }
+
+    public static boolean checkAnagram(String words, String target){
+        if (words.length() != target.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char c:words.toCharArray()){
+            if (!charCount.containsKey(c)){
+                charCount.put(c, 1);
+            } else {
+                int currentValue = charCount.get(c);
+                charCount.put(c, currentValue + 1);
+            }
+        }
+
+        for (char c:target.toCharArray()){
+            if (!charCount.containsKey(c)){
+                return false;
+            }
+            int currentValue = charCount.get(c);
+            if (currentValue == 1){
+                charCount.remove(c);
+            } else {
+                charCount.put(c, currentValue - 1);
+            }
+        }
+
+        return charCount.isEmpty();
 
     }
 }
