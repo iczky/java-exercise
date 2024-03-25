3f28e144bb894bffde6a35ab1f2e1f637a2ff131
iczky <fiqra22@gmail.com>
2024-03-25 10:21:00 +0700

    feat: Complete Exercise 2

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,15 +12,17 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
-
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        rotateArray(numbers, 5);
     }
 
-    public static void rotateArray(int[] listNumber, int target){
-        for (int i = 0; i < listNumber.length; i++) {
-            int left = i;
-            int right = listNumber.length - 1;
-
-
+    public static void rotateArray(ArrayList<Integer> listNumber, int target){
+        for (int i = 0; i < target; i++) {
+            listNumber.add(listNumber.getFirst());
+            listNumber.remove(listNumber.getFirst());
+            System.out.println("I " + listNumber);
         }
+
+        System.out.println(listNumber);
     }
 }
