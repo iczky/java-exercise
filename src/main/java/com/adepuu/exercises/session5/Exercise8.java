35de6aff4395bab8fcbf7bac1a373b13b0c5884c
iczky <fiqra22@gmail.com>
2024-03-25 13:32:32 +0700

    feat: Complete Exercise 8

--- src/main/java/com/adepuu/exercises/session5/Exercise8.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise8.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Exercise8 {
     /**
      * Java String Program to Find all Duplicates on Array
@@ -17,6 +20,30 @@ public class Exercise8 {
      * Output: []
      */
     public static void main(String[] args) {
+        int[] numbers = {4,3,2,7,8,2,3,1};
+        System.out.println("The result is: " + findDuplicate(numbers));
+    }
 
+    public static ArrayList<Integer> findDuplicate(int[] listNumbers){
+        HashMap<Integer,Integer> duplicateCount= new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int number:listNumbers){
+            if (!duplicateCount.containsKey(number)){
+                duplicateCount.put(number, 1);
+            } else {
+                duplicateCount.put(number, 2);
+            }
+        }
+
+        duplicateCount.forEach(
+                (key, value) -> {
+                    if (value == 2){
+                        result.add(key);
+                    }
+                }
+        );
+
+        return result;
     }
 }
